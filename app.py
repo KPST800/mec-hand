094e939255fcf4a9abca8081f8a9a47d03e79693
jylee1234 <42171559+jylee1234@users.noreply.github.com>
2020-07-14 19:02:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,50 +27,8 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    # return Response('ETRI Object Detection Test 2019.09.27 #8')
-    return render_template('index.html')
-
-
-def gen(cam_):
-    while True:
-        #get camera frame
-        send_im = 0#0:output image, 1: json
-        frame_output = cam_.getFrame(send_im)
-        if send_im:
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame_output + b'\r\n\r\n')
-        else:
-            #example
-            print (frame_output)
-            print("aegaweg")
-
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/local')
-def local():
-    return Response(open('./static/local.html').read(), mimetype="text/html")
-
-
-@app.route('/video')
-def remote():
-    return Response(open('./static/video.html').read(), mimetype="text/html")
-
-
-@app.route('/test')
-def test():
-    PATH_TO_TEST_IMAGES_DIR = 'object_detection/test_images'  # cwh
-    TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3)]
-
-    img = cv2.imread(TEST_IMAGE_PATHS)
-    img_reszd = cv2.resize(img, (w_hand, h_hand))
-    img_pre_hand = ((img_reszd - 127.5) /  127.5).astype('float32')
-    output_tensors = model_hand_lm.runModel(img_pre_hand)
-    output_json = hand_json(output_tensors, [img.shape[0],img.shape[1]], [h_hand, w_hand])
-    return output_json
+    return Response('ETRI Hand Tracking Test 2020.07.14 #1')
+    #return render_template('index.html')
 
 
 @app.route('/image', methods=['POST'])
